8c8d021fb579d102cc37e13f0671564e2a567edb
Alexander <onepopov@yandex.ru>
2023-01-25 20:11:29 +0400

    Fix rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      external: "Buffer",
       plugins: [inject({ Buffer: ["Buffer", "Buffer"]})]
     }
   }
